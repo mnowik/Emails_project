2a8a21f036875c15fae6ddf4ae660bb2b1e8b320
Matthieu Nowicki <nowicki.matthieu@gmail.com>
2014-03-18 17:44:57 -0700

    Login + mails

--- application/main.py
+++ application/main.py
@@ -1,22 +1,52 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,json
 app = Flask(__name__)
 
 #---------------contextio loader------------------
 import contextio as c
 
-'''CONSUMER_KEY = 'key'
+CONSUMER_KEY = 'key'
 CONSUMER_SECRET = 'pass'
-ID = 'id' '''
+ID = 'id' 
+
 
 context_io = c.ContextIO(
-	consumer_key=CONSUMER_KEY, 
+	consumer_key=CONSUMER_KEY,
 	consumer_secret=CONSUMER_SECRET
 	)
 
 #---------------template in html---------------
-@app.route('/')
-def hello(name=None):
+@app.route('/messages')
+def messages(name=None):
     return render_template('slides.html', emails=messages())
+
+
+@app.route('/login')
+def login(name=None):
+    return render_template('login.html')
+
+
+@app.route('/login1', methods=['GET', 'POST'])
+def login1():
+    return 'bonjour'
+
+
+
+
+@app.route('/login2', methods=["GET", "POST"])
+def login2():
+    if request.method == "POST":
+    	accounts = context_io.post_accounts()
+	# since we return a list, let's be sure we have a result
+	if accounts:
+	    account = accounts[0]
+
+	params = {
+	   'email': request.data
+	}
+	account = c.Account(context_io, params)
+	account.POST()
+
+        return account
 
 
 
@@ -47,5 +77,6 @@ def messages():
 	return emails
 
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
